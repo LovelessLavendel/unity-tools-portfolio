e7592fb6c883e019744061d863860db3c414af2b
Loveless Lavendel <50579773+LovelessLavendel@users.noreply.github.com>
2026-09-09 06:54:28 +0200

    Add files via upload

---
+++ js/klapp-funktion.js
@@ -0,0 +1,14 @@
+﻿function toggleCode(blockId) {
+    const block = document.getElementById(blockId);
+    const btn = block.querySelector('.toggle-code-btn');
+
+    if (block.classList.contains('collapsed')) {
+        block.classList.remove('collapsed');
+        btn.innerText = '▲ Weniger anzeigen (Code einklappen)';
+    } else {
+        block.classList.add('collapsed');
+        btn.innerText = '▼ Mehr anzeigen (vollständigen Code ausklappen)';
+        // Scrollt den Nutzer sanft zurück nach oben zum Start des jeweiligen Skripts
+        block.scrollIntoView({ behavior: 'smooth' });
+    }
+}
